968daee164f7b872a852c999900f4deb9337b06e
Ron Enubs <ronenubs@gmail.com>
2020-11-04 10:57:11 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(int number in SomeNumbers())
+            {
+                Console.Write(number.ToString() + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static System.Collections.IEnumerable SomeNumbers()
+        {
+            yield return 3;
+            yield return 5;
+            yield return 8;
+        }
+    }
+}
